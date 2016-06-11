273ce24cd28811d6b1e1c472e9c4444d2d40e751
Kylee Tilley <kyleetilley@gmail.com>
2016-06-11 17:26:57 -0500

    Changing how src/index.js exports to fix module import issue

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import factory from './factory';
-export { default as template } from './template';
-export { default as render } from './render';
+import template from './template';
+import render from './render';
 
-export default factory;
+module.exports = { factory, template, render };
