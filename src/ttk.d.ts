191d60781ddddbe74c72a3ba6a9a9e293ff760c1
Daniel.Rosenwasser@microsoft.com <drosen@microsoft.com>
2016-06-17 13:01:24 -0700

    Fixed declaration file to be a proper module.

--- src/ttk.d.ts
+++ src/ttk.d.ts
@@ -1,58 +1,57 @@
-declare module 'ttk' {
-  type middlewareFn = (value:string) => string;
 
-  interface factoryOptions {
-    keyPrefix?:string,
-    mergeMiddleware?:boolean,
-    valueFns?: middlewareFn[],
-    renderFns?: middlewareFn[]
-  }
+type middlewareFn = (value:string) => string;
 
-  type defaultOptions = {
-    keyPrefix:string;
-    mergeMiddleware:boolean;
-    valueFns: middlewareFn[];
-    renderFns: middlewareFn[];
-  }
-
-  interface middlewareStack {
-    [s:string]:middlewareFn;
-  }
-
-  interface valueMiddlewareStack extends middlewareStack {
-    /** Wraps values for use in sql statements */
-    sqlValueWrapper:middlewareFn;
-  }
-  
-  interface renderMiddlewareStack extends middlewareStack {
-    /** Trims multiline strings and normalizes indentation */
-    multilineStringTrim:middlewareFn;
-  }
-
-  type factoryFn = (options:factoryOptions) => templateFn;
-  type templateFn = (strings:string[], keys?:any[]) => renderFn;
-  type renderFn = (context:Object) => string;
-  
-  interface ttk {
-    /** Returns a new template function with options applied */
-    factory:factoryFn;
-    /** Tagged template literal function */
-    template:templateFn;
-    /** Render template to string using context data */
-    render:renderFn;
-    /** Default options for factory */
-    defaultOptions: defaultOptions;
-    
-    /** Middleware stacks */
-    middleware: {
-      /** Value middleware stack */
-      value:valueMiddlewareStack,
-      /** Render middleware stack */
-      render:renderMiddlewareStack
-    }
-  }
-
-  var ttk:ttk;
-
-  export = ttk;
+interface factoryOptions {
+  keyPrefix?:string,
+  mergeMiddleware?:boolean,
+  valueFns?: middlewareFn[],
+  renderFns?: middlewareFn[]
 }
+
+type defaultOptions = {
+  keyPrefix:string;
+  mergeMiddleware:boolean;
+  valueFns: middlewareFn[];
+  renderFns: middlewareFn[];
+}
+
+interface middlewareStack {
+  [s:string]:middlewareFn;
+}
+
+interface valueMiddlewareStack extends middlewareStack {
+  /** Wraps values for use in sql statements */
+  sqlValueWrapper:middlewareFn;
+}
+
+interface renderMiddlewareStack extends middlewareStack {
+  /** Trims multiline strings and normalizes indentation */
+  multilineStringTrim:middlewareFn;
+}
+
+type factoryFn = (options:factoryOptions) => templateFn;
+type templateFn = (strings:string[], keys?:any[]) => renderFn;
+type renderFn = (context:Object) => string;
+
+interface ttk {
+  /** Returns a new template function with options applied */
+  factory:factoryFn;
+  /** Tagged template literal function */
+  template:templateFn;
+  /** Render template to string using context data */
+  render:renderFn;
+  /** Default options for factory */
+  defaultOptions: defaultOptions;
+
+  /** Middleware stacks */
+  middleware: {
+    /** Value middleware stack */
+    value:valueMiddlewareStack,
+    /** Render middleware stack */
+    render:renderMiddlewareStack
+  }
+}
+
+declare var ttk: ttk;
+
+export = ttk;
